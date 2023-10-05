d9e2b4cd2036161263ab61ce017cec918890fb93
valinadz <valinaadzra@gmail.com>
2023-10-05 07:54:37 +0700

    jobsheets 6

---
+++ Pemilihan2Percobaan122.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan122 {
+    public static void main (String []args) {
+
+        Scanner input22 = new Scanner(System.in);
+
+        System.out.println("Masukkan tahun:  ");
+        int tahun = input22.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat"); 
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
